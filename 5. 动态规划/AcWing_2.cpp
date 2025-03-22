82d3cb7a18dafeb73f1c323585111a0470f32c38
cherry77-cloud <cherry1615405@gmail.com>
2025-03-22 13:21:50 +0800

    Update and rename AcWing_002.py to AcWing_2.cpp

---
+++ 5. 动态规划/AcWing_2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <functional>
+
+using namespace std;
+
+int zero_one_knapsack(const vector<int>& volumes, const vector<int>& weights, int capacity) 
+{
+    vector<int> dp(capacity + 1, 0);
+    
+    for (int i = 0; i < volumes.size(); ++i) {
+        for (int j = capacity; j >= volumes[i]; --j) {
+            dp[j] = max(dp[j], dp[j - volumes[i]] + weights[i]);
+        }
+    }
+    return dp[capacity];
+}
+
+
+int main() 
+{
+    int n, capacity;
+    cin >> n >> capacity;
+    
+    vector<int> volumes(n);
+    vector<int> weights(n);
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> volumes[i] >> weights[i];
+    }
+
+    cout << zero_one_knapsack(volumes, weights, capacity) << endl;
+    return 0;
+}
