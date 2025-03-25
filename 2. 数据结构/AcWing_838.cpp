e6612ed858fc4b97f307041dd45419ac0584ea7d
cherry77-cloud <cherry1615405@gmail.com>
2025-03-25 16:05:47 +0800

    Update and rename AcWing_0838.py to AcWing_838.cpp

---
+++ 2. 数据结构/AcWing_838.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+#include <algorithm>
+
+using namespace std;
+
+class MinHeap {
+private:
+    vector<int> heap;
+
+    void heapifyUp(int p) {
+        while (p > 0 && heap[p] < heap[(p - 1) / 2]) {
+            std::swap(heap[p], heap[(p - 1) / 2]);
+            p = (p - 1) / 2;
+        }
+    }
+
+    void heapifyDown(int u) {
+        int size = heap.size();
+        while (true) {
+            int left = 2 * u + 1, right = 2 * u + 2, smallest = u;
+            if (left < size && heap[left] < heap[smallest]) smallest = left;
+            if (right < size && heap[right] < heap[smallest]) smallest = right;
+            if (smallest == u) break;
+            swap(heap[u], heap[smallest]);
+            u = smallest;
+        }
+    }
+
+public:
+    void push(int value) {
+        heap.push_back(value);
+        heapifyUp(heap.size() - 1);
+    }
+
+    void pop() {
+        heap[0] = heap.back();
+        heap.pop_back();
+        if (!heap.empty()) heapifyDown(0);
+    }
+
+    int top() const {
+        return heap[0];
+    }
+
+    bool empty() const { return heap.empty(); }
+    size_t size() const { return heap.size(); }
+};
+
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+
+    MinHeap heap;
+
+    for (int i = 0; i < n; ++i) {
+        int x;
+        std::cin >> x;
+        heap.push(x);
+    }
+
+    for (int i = 0; i < m; ++i) {
+        std::cout << heap.top() << " ";
+        heap.pop();
+    }
+
+    return 0;
+}
