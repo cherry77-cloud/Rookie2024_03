cead425ecf41a81d45f6cd443593057f359a0ee8
cherry77-cloud <cherry1615405@gmail.com>
2025-03-25 22:35:47 +0800

    Update AcWing_849.cpp

--- 3. 搜索与图论/AcWing_849.cpp
+++ 3. 搜索与图论/AcWing_849.cpp
@@ -1,54 +1,55 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <climits>
-
 using namespace std;
 
 constexpr int INF = 0x3f3f3f3f;
 
-
-int dijkstra(const vector<vector<int>>& g, int n) {
-    vector<int> dist(n + 1, INF);
-    vector<bool> visited(n + 1, false);
-    dist[1] = 0;
+int dijkstra(const vector<vector<int>>& g) {
+    int n = g.size();
+    vector<int> dist(n, INF);
+    vector<bool> visited(n, false);
     
-    for (int i = 1; i < n; ++i) {
-        int u = 0;
-        for (int j = 1; j <= n; ++j) {
-            if (!visited[j] && (u == 0 || dist[j] < dist[u])) {
+    dist[0] = 0;
+    
+    for (int i = 0; i < n - 1; ++i) {
+        int u = -1;
+        for (int j = 0; j < n; ++j) {
+            if (!visited[j] && (u == -1 || dist[j] < dist[u])) {
                 u = j;
             }
         }
         
-        if (u == 0 || dist[u] == INF) break;
+        if (u == -1) break;
         visited[u] = true;
         
-        for (int v = 1; v <= n; ++v) {
-            dist[v] = min(dist[v], dist[u] + g[u][v]);
+        // 松弛操作
+        for (int v = 0; v < n; ++v) {
+            if (dist[u] + g[u][v] < dist[v]) {
+                dist[v] = dist[u] + g[u][v];
+            }
         }
     }
     
-    return dist[n] == INF ? -1 : dist[n];
+    return dist[n-1] == INF ? -1 : dist[n-1];
 }
 
 int main() {
     int n, m;
     cin >> n >> m;
     
-    vector<vector<int>> g(n + 1, vector<int>(n + 1, INF));
-    for (int i = 1; i <= n; ++i) {
+    vector<vector<int>> g(n, vector<int>(n, INF));
+    for (int i = 0; i < n; ++i) {
         g[i][i] = 0;
     }
     
     for (int i = 0; i < m; ++i) {
-        int u, v, w;
-        cin >> u >> v >> w;
-        g[u][v] = min(g[u][v], w);
+        int x, y, z;
+        cin >> x >> y >> z;
+        g[x-1][y-1] = min(g[x-1][y-1], z);
     }
     
-    int shortest = dijkstra(g, n);
-    cout << shortest << endl;
+    cout << dijkstra(g) << endl;
     
     return 0;
 }
