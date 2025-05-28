c31b09fb8dca44410889c8f647aca8a700b7f66c
cherry77-cloud <cherry1615405@gmail.com>
2025-05-29 00:31:30 +0800

    Update and rename AcWing_866.py to AcWing_866.cpp

---
+++ 4. 数学知识/AcWing_866.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+bool is_prime(int n) {
+    if (n < 2)  return false;
+    for (int i = 2; i <= n / i; ++i) {
+        if (n % i == 0)  return false;
+    }
+    return true;
+}
+
+int main() {
+    
+    int n;  cin >> n;
+    
+    while (n--) {
+        int x;  cin >> x;
+        if (is_prime(x))  puts("Yes");
+        else  puts("No");
+    }
+    
+    return 0;
+}
